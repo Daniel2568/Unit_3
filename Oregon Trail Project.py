249e3eb88ba3aba9c1d04bb6a88f5236827012d2
Daniel Worth <dworth25@students.bmchs.org>
2022-03-21 09:40:45 -0500

    ehhhhhhhhhh

---
+++ Oregon Trail Project.py
@@ -0,0 +1,7 @@
+# Oregon Trail Game
+print()
+print("Welcome to Oregon Trail!")
+print()
+print("*This is a game that is made up of text*")
+print()
+print("You are currently living in Independence, Missouri. You trying to make it to Oregon City,Oregon by December 31st\nIt is currently 03/01 and you will have to travel 2,000 miles, you will be starting off with 500lbs of food, and you will start with 5 health... good luck.")
