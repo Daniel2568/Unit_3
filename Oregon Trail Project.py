86f1b4b3890f6f4cc56abda4a380813c9bd92c48
Daniel Worth <dworth25@students.bmchs.org>
2022-03-25 09:53:49 -0500

    almost doneee

--- Oregon Trail Project.py
+++ Oregon Trail Project.py
@@ -1,5 +1,7 @@
 # Oregon Trail Game
 
+import random
+
 #introduction
 print()
 print("Welcome to Oregon Trail!")
@@ -11,52 +13,113 @@ print("You are currently living in Independence, Missouri. You trying to make it
 # User Customization/Info
 player_name = input("What would you like your name to be?>")
 
-import random 
-#health status
-
+#global variables
+MONTHS = ['dumby', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+MONTHS_WITH_30_DAYS = ['April', 'June', 'September', 'November']
 health = 5
 food = 500
 miles_to_go = 2000
 current_day = 1
-currnt_month = 3
+current_month = 3
 
 
-#Player choices 
-print("What action would you like to perform?(You can travel, rest, hunt, status, help, quit)")
+#Player Functions
+
+
 
 def travel():
-    global miles_to_go 
+    global miles_to_go, current_day
     
-    miles_travled = random.randint(30,60)
-    print(f"You've travel {miles_travled} miles")
+    days_this_travel = random.randint(3,7)
+    miles_traveled  = random.randint(30,60)
 
-    miles_to_go -= miles_travled
+    print(f"You have traveled {miles_traveled} miles")
+    current_day += days_this_travel
 
-def rest():
-    pass
+    miles_to_go = miles_traveled
 
-def hunt():
-    pass
+    add_day(days_this_travel)
+
+def add_day(days):
+    global current_day, current_month
+    current_day += days 
+
+
+# Month rule
+    if MONTHS[current_month] in MONTHS_WITH_30_DAYS:
+        if current_day > 30:
+            current_day -= 30
+            current_month += 1
+    
+    else:
+        if current_day > 31:
+            current_day -= 31
+            current_month += 1
+
+    consume_food(days)
+
+def consume_food(days):
+    global food
+
+    food_consumed_this_turn = 5 * days
+    
+     
+
 
 def status():
-    pass
+    print(f"You have {miles_to_go} miles to go")
+    print()
+    print(f"It is {MONTHS[current_month]} {current_day}.")
+    print()
+    print(f"You have {health} remaining health")
+    print()
+    print(f"You have {food} remaining food")
+
+def rest():
+    global health
+    days_this_rest = random.randint(2,5)
+    print(f"You rested for {days_this_rest} days")
+    health += 1
+    if health > 5:
+        print("You probably shouldnt rest if your health is full\nYou cant have more than 5 health")
+    else:
+        health += 1
+
+    add_day(days_this_rest)
+
+def hunt():
+    global food
+
+    days_this_hunt = random.randint(2,5)
+    food += 100
+
+    add_day(days_this_hunt)
 
 def help():
-    print("Your goal is to get to Oregon City, Oregon by 12/31 just before winter starts or you die\n you can travel, rest, hunt, ask for help, check your player status, or quit\n")
+    print("Your goal is to get to Oregon City, Oregon by 12/31 just before winter starts or you die\nRemember you can travel, rest, hunt, ask for help, check your player status, or quit\n There are chances you can die from wild animals and or weather as well")
 
 def quit():
-    pass
-
-def eat():
-    pass
-    global food
-    food -= 5 
-    print(f"You've consumed 5lbs of food")
+    global game_over
+    print("Hope you enjoyed the game up to this point, have a nice day")
+    game_over = True
 
 #Game loop 
-while True:
-    pass
-    user_choice = input("")
+game_over = False
+
+while not game_over:
+    user_choice = input("What action would you like to perform?>")
+
+    if user_choice == 'travel':
+        travel()
+    if user_choice == 'status':
+        status()
+    elif user_choice == 'hunt':
+        hunt()
+    elif user_choice == 'quit':
+        quit()
+    else: 
+        print("Im not sure I understood your action choice")
+
 
 
 
